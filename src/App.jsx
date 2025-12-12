821f48b5601436bbee9b76fc4fd9af08e37c4798
Ana Carolina Moreira Machado <carolina.machado@ipvc.pt>
2025-12-12 09:53:23 +0000

    fix: react-router-dom and fix routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-// src/App.jsx
 import { Navigate } from "react-router-dom";
 
 function App() {
-  // sempre que alguém vai a "/", vai direto para /login
   return <Navigate to="/login" replace />;
 }
 
